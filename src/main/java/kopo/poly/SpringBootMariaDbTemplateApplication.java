8a3dee6e0c61fb117f94889671c9b5b105f792f3
Hyeopgeon Lee <39357722+Hyeopgeon-Lee@users.noreply.github.com>
2023-01-02 21:58:40 +0900

    Initial commit

---
+++ src/main/java/kopo/poly/SpringBootMariaDbTemplateApplication.java
@@ -0,0 +1,13 @@
+package kopo.poly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMariaDbTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMariaDbTemplateApplication.class, args);
+    }
+
+}
---
+++ src/main/java/kopo/poly/service/impl/NoticeService.java
@@ -0,0 +1,74 @@
+package kopo.poly.service.impl;
+
+import kopo.poly.dto.NoticeDTO;
+import kopo.poly.persistance.mapper.INoticeMapper;
+import kopo.poly.service.INoticeService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class NoticeService implements INoticeService {
+
+    // RequiredArgsConstructor 어노테이션으로 생성자를 자동 생성함
+    // noticeMapper 변수에 이미 메모리에 올라간 INoticeMapper 객체를 넣어줌
+    // 예전에는 autowired 어노테이션를 통해 설정했었지만, 이젠 생성자를 통해 객체 주입함
+    private final INoticeMapper noticeMapper;
+
+    @Override
+    public List<NoticeDTO> getNoticeList() throws Exception {
+
+        log.info(this.getClass().getName() + ".getNoticeList start!");
+
+        return noticeMapper.getNoticeList();
+
+    }
+
+    @Transactional
+    @Override
+    public void InsertNoticeInfo(NoticeDTO pDTO) throws Exception {
+
+        log.info(this.getClass().getName() + ".InsertNoticeInfo start!");
+
+        noticeMapper.InsertNoticeInfo(pDTO);
+    }
+
+    @Transactional
+    @Override
+    public NoticeDTO getNoticeInfo(NoticeDTO pDTO) throws Exception {
+
+        log.info(this.getClass().getName() + ".getNoticeInfo start!");
+
+        // 상세보기 할때마다, 조회수 증가하기
+        log.info("Update ReadCNT");
+        noticeMapper.updateNoticeReadCnt(pDTO);
+
+        return noticeMapper.getNoticeInfo(pDTO);
+
+    }
+
+    @Transactional
+    @Override
+    public void updateNoticeInfo(NoticeDTO pDTO) throws Exception {
+
+        log.info(this.getClass().getName() + ".updateNoticeInfo start!");
+
+        noticeMapper.updateNoticeInfo(pDTO);
+
+    }
+
+    @Transactional
+    @Override
+    public void deleteNoticeInfo(NoticeDTO pDTO) throws Exception {
+
+        log.info(this.getClass().getName() + ".deleteNoticeInfo start!");
+
+        noticeMapper.deleteNoticeInfo(pDTO);
+
+    }
+}
